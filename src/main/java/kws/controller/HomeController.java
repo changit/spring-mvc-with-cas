85de4b6e8f686fb57bf87793318d4d05c6e831a6
Sampath Chandana <kwsampath@gmail.com>
2011-08-14 13:41:22 +0530

    first commit

---
+++ src/main/java/kws/controller/HomeController.java
@@ -0,0 +1,35 @@
+package kws.controller;
+
+import javax.persistence.PersistenceContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kws.service.EntityService;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@Autowired
+	EntityService entityService;
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String home() {
+	    	logger.info("requesting home");
+		return "home";
+	}
+
+}
+
